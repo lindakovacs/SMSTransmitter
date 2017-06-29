a7bea4c2039e57963cfd13bf15e13de245b3027e
MedUnes <contact@medunes.net>
2017-06-29 15:29:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,15 +3,6 @@ use SMSTransmitter\Controller\FrontController;
 
 require_once 'app/start.php';
 
-
-        /*$Message             = new \MessageBird\Objects\Message();
-        $Message->originator = 'MessageBird';
-        $Message->recipients = array("+21696390001");
-        $Message->body       = 'This is a test message.';
-        $Message->setBinarySms(0x050003CC0101, "123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 ");
-        $messageBirdClient = new \MessageBird\Client("aBwzuetKTJiaXmYc0CKqEepUQ");
-        $messageResult = $messageBirdClient->messages->create($Message);
-        echo json_encode($messageResult);*/
 $frontController = new FrontController();
 
 $frontController->indexAction();
